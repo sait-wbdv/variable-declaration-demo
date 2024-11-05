64a86c63513fc0cd5b54419d278098bb2a5cd16c
Ashlyn Knox <ashlyn@lilyx.io>
2024-11-05 10:35:57 -0700

    optimized loop example

--- loop.js
+++ loop.js
@@ -1,12 +1,7 @@
 /* Simple loop example */
 
 const arr = ["macbook pro", "iphone 16", "iphone 16 pro"];
-/*
-for (let i = 0; i <= arr.length - 1; i++) {
-  console.log(arr[i]);
-}
-*/
 
-for (let i = 0; i <= arr.length; i++) {
-  console.log(arr[i]);
+for (let i = 0; i < arr.length; i++) {
+  console.log(i, arr[i]);
 }
