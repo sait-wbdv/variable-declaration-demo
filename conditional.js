de9357897669528c814bab737755a6f0fc575efc
Ashlyn Knox <ashlyn@lilyx.io>
2024-11-05 10:26:30 -0700

    super simple conditional example

---
+++ conditional.js
@@ -0,0 +1,7 @@
+const userName = "Nobody";
+
+if (userName != "Nobody") {
+  console.log(`Incorrect: The user is named ${userName}`);
+} else {
+  console.log(`Correct: The user name is ${userName}`);
+}
